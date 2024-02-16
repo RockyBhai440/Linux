6281bebd82e1a86fdc7613c0f633b52c96ea5dc1
RockyBhai440 <155788812+RockyBhai440@users.noreply.github.com>
2024-02-16 13:52:51 +0600

    Delete All Code/Ashraf Sir directory

--- All Code/Ashraf Sir/add.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include"header.h"
-
-void add(int a, int b){
-	printf("\nRsult = %d\n", a+b+10);
-
-}
--- All Code/Ashraf Sir/main.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include"header.h"
-int main(){
-	int a, b;
-	printf("Enter two integer number: ");
-	scanf("%d %d", &a, &b);
-	add(a, b);
-}
