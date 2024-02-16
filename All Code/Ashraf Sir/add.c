55fb17c4a76873c9a5da216a19e22c68eb79bca7
RockyBhai440 <155788812+RockyBhai440@users.noreply.github.com>
2024-02-16 13:50:20 +0600

    Ashraf Sir Done

---
+++ All Code/Ashraf Sir/add.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include"header.h"
+
+void add(int a, int b){
+	printf("\nRsult = %d\n", a+b+10);
+
+}
---
+++ All Code/Ashraf Sir/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"header.h"
+int main(){
+	int a, b;
+	printf("Enter two integer number: ");
+	scanf("%d %d", &a, &b);
+	add(a, b);
+}
